0604f52ec317dfe50ab14630798d2796ae94da65
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 23:42:32 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,5 +1,20 @@
-const todoReducer = (state, action) => {
+/*const todoReducer = (state, action) => {
     switch (action.type) {
+
+    }
+}
+
+export { todoReducer }*/
+
+const todoReducer = (state, action) => {
+    console.log("Reducer", state, action)
+    switch (action.type) {
+        case "ADD":
+            console.log("ADD Click")
+            return [...state, { title: action.payload.todoTitle, id: action.payload.todoId }];
+
+        case "DELETE":
+            return state.filter((item) => item.id !== action.payload.todoId);
 
     }
 }
