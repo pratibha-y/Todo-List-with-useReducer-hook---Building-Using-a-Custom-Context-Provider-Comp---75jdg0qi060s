b22915823ca15e3243f15045a2cec04961c3732d
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 23:53:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,31 +15,26 @@ const App = () => {
   )
 }*/
 
-import React, { useReducer } from 'react'
-import { todoReducer } from '../reducers/todo';
-import '../styles/App.css';
-import { AddTodo } from './AddTodo';
-import { Todo } from './Todo';
+import React, { useReducer } from "react";
+import { todoReducer } from "../reducers/todo";
+import "../styles/App.css";
+import { AddTodo } from "./AddTodo";
+import { Todo } from "./Todo";
 
-export const myContext = React.createContext();
 const App = () => {
-  const [state, dispatch] = useReducer(todoReducer, [])
-
+  const [state, dispatch] = useReducer(todoReducer, []);
 
   return (
-    <myContext.Provider value={{ todoState: state, dispatchFun: dispatch }}>
-      <div id="main">
-        <AddTodo dispatch={dispatch} />
-        <ul>
-          {state.map((todo) => <Todo todoObj={todo} />)}
-        </ul>
-      </div>
-    </myContext.Provider>
-  )
-}
-
+    <div id="main">
+      <AddTodo dispatch={dispatch} />
+      {state.map((todo) => (
+        <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
 
 
-export default App;
+
