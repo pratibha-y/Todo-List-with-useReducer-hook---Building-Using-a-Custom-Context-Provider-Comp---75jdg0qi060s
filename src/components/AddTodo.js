584fb7f092960fd3e80a5ba21e2de71534392d1a
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 23:36:18 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,40 @@
-import React from "react"
+/*import React from "react"
 
 const AddTodo = ({ dispatch }) => {
 
 
     return (
         <></>
+    )
+}
+
+export { AddTodo }*/
+
+import React, { useState } from "react"
+import { useContext } from "react";
+import { myContext } from "./App";
+
+const AddTodo = ({ dispatch }) => {
+    const newContext = useContext(myContext);
+    console.log("Dispatch", newContext);
+    const [inputValue, setInputValue] = useState("");
+
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        newContext.dispatchFun({ type: "ADD", payload: { todoTitle: inputValue, todoId: Date.now() } })
+        setInputValue("");
+    }
+
+    return (
+        <>
+            <form id="todo-form" onSubmit={handleForm}>
+                <input type={"text"} id="todo-input" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                {/* <input type="submit" value="ADD TODO" /> */}
+                <button type="submit">Add Todo</button>
+
+            </form>
+        </>
     )
 }
 
