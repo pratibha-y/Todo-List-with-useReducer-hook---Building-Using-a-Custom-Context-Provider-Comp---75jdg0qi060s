04443cbae281f8cf5cd2a60598fe02aa18cb1d0c
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-04-08 23:41:23 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,25 @@
-import React from 'react';
+/*import React from 'react';
 const Todo = () => {
     return (
         <></>
+    )
+}
+
+
+export { Todo }*/
+
+import React from 'react';
+import { useContext } from 'react';
+import { myContext } from './App';
+const Todo = ({ todoObj }) => {
+    console.log(todoObj);
+    const newContext = useContext(myContext);
+    return (
+        <div className='todo'>
+            <div className="todo-title">{todoObj.title}</div>
+            <button className='todo-delete' onClick={(e) => newContext.dispatchFun({ type: "DELETE", payload: { todoTitle: todoObj.title, todoId: todoObj.id } })}>Delete</button>
+
+        </div>
     )
 }
 
